1be07f2937402acc9d5fadd541b0bc2182a185cf
hayashi <hayashinoshun@gmail.com>
2022-11-07 23:44:40 +0900

    タスク#1 入力可能＋表示まで完了

--- test1.java
+++ test1.java
@@ -1,7 +1,13 @@
 package java_joint;
 
+import java.util.Scanner;
+
 class test1 {
 	public static void main(String[] args) {
-	  System.out.println("こんにちは");
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("あなたのお名前は？: ");
+		String name = scanner.next();
+	    System.out.println("こんにちは" + name + "さん");
 	}
 }

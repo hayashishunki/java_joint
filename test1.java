d7c741d9bb8eefa7551d6d7f0392057201c0fc96
hayashi <hayashinoshun@gmail.com>
2022-11-08 00:01:22 +0900

    インデント調整

--- test1.java
+++ test1.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 class test1 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		
+
 		System.out.print("あなたのお名前は？: ");
 		String name = scanner.next();
-	    System.out.println("こんにちは" + name + "さん");
+		System.out.println("こんにちは" + name + "さん");
 	}
 }

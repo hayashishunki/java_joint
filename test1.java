5f463f3eaed45ae2c4c32b29eb02ee24032a1c4c
hayashi <hayashinoshun@gmail.com>
2022-11-09 00:46:17 +0900

    タスク#4占いゲーム作成完了 ランダム整数を作成し当てはまる値を表示

--- test1.java
+++ test1.java
@@ -1,13 +1,37 @@
 package java_joint;
 
+import java.util.Random;
 import java.util.Scanner;
 
 class test1 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		Random random = new Random();
 
-		System.out.print("あなたのお名前は？: ");
+		System.out.println("名前からあなたを占います");
+		System.out.print("あなたのお名前は: ");
 		String name = scanner.next();
-		System.out.println("こんにちは" + name + "さん");
+		if (name != null) {
+			int num = random.nextInt(4);
+			System.out.println("こんにちは" + name + "さん");
+			System.out.print("あなたの運勢は: ");
+			switch (num) {
+			case 0:
+				System.out.println("大吉です");
+				break;
+			case 1:
+				System.out.println("中吉です");
+				break;
+			case 2:
+				System.out.println("吉です");
+				break;
+			default:
+				System.out.println("凶です");
+				break;
+			}
+		} else {
+			System.out.println("エラーですよ");
+		}
+
 	}
 }
